b2c762872ba7d136941c6e5bd1186179d00044d5
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-04-07 18:05:21 +0200

    delegate with optional parameter exception demo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("FunctionWithOptional");
+RunTaskSuccess(FunctionWithOptional);
+Thread.Sleep(100);
+
+Console.WriteLine("FunctionWithOptional");
+await RunTaskFails(FunctionWithOptional);
+Thread.Sleep(100);
+
+async Task RunTaskFails(Delegate task)
+{
+    await Task.Run(() => task.DynamicInvoke());
+}
+
+void RunTaskSuccess(Delegate task)
+{
+    Task.Run(() => task.DynamicInvoke());
+}
+
+async Task FunctionWithOptional(string? parameter = null)
+{
+    Console.WriteLine("Method ran! " + parameter);
+}
